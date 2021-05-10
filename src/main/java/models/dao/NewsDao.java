cdfcb798cc36a679ceb06adfc58db4b1a0e3be7e
Sheila Sharon <devsheilawambui@gmail.com>
2021-05-10 23:56:55 +0300

    add news interface

--- src/main/java/models/dao/NewsDao.java
+++ src/main/java/models/dao/NewsDao.java
@@ -1,4 +1,19 @@
 package models.dao;
 
+import models.DepNews;
+import models.Departments;
+
+import java.util.List;
+
 public interface NewsDao {
+
+    void add (Departments department);
+    void addDepartmentToNews(Departments department, DepNews news);
+
+    List<Departments> all();
+    List<DepNews> getAllNewsDepartments(int newsId);
+
+    Departments findById(int id);
+    void deleteById(int id);
+    void clearAll();
 }
