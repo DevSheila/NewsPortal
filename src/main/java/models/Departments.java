15b32eed13c1931b525ff8c30ce86d43fc22f1c3
Sheila Sharon <devsheilawambui@gmail.com>
2021-05-10 21:07:03 +0300

    generate department getters and setters

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -1,9 +1,61 @@
 package models;
 
+import java.util.Objects;
+
 public class Departments {
     private int id;
     private String name;
     private String description;
     private int numEmployees;
 
+    public Departments(String name, String description, int numEmployees) {
+        this.name = name;
+        this.description = description;
+        this.numEmployees = numEmployees;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getNumEmployees() {
+        return numEmployees;
+    }
+
+    public void setNumEmployees(int numEmployees) {
+        this.numEmployees = numEmployees;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Departments)) return false;
+        Departments that = (Departments) o;
+        return getId() == that.getId() && getNumEmployees() == that.getNumEmployees() && Objects.equals(getName(), that.getName()) && Objects.equals(getDescription(), that.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getDescription(), getNumEmployees());
+    }
 }
