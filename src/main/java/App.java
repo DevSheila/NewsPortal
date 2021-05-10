8298205f160d9cc2c4b86ad396e3355b1b8b741a
Sheila Sharon <devsheilawambui@gmail.com>
2021-05-11 01:19:55 +0300

    route to adda new user

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,10 @@
 import com.google.gson.Gson;
 import exceptions.ApiException;
+import models.Users;
 import models.dao.Sql2oDepNewsDao;
 import models.dao.Sql2oDepartmentDao;
 import models.dao.Sql2oGeneralNewsDao;
+import models.dao.Sql2oUsersDao;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
@@ -16,6 +18,8 @@ public class App {
         Sql2oDepartmentDao departmentDao;
         Sql2oDepNewsDao depNewsDao;
         Sql2oGeneralNewsDao generalNewsDao;
+        Sql2oUsersDao usersDao;
+
         Connection conn;
         Gson gson = new Gson();
 
@@ -24,6 +28,17 @@ public class App {
         String connectionString = "jdbc:postgresql://localhost:5432/news_portal";   //connect to jadle, not jadle_test! try not to copy paste
         Sql2o sql2o = new Sql2o(connectionString, "postgres", null);
 
+        departmentDao = new Sql2oDepartmentDao(sql2o);
+        depNewsDao = new Sql2oDepNewsDao(sql2o);
+        generalNewsDao = new Sql2oGeneralNewsDao(sql2o);
+        usersDao = new Sql2oUsersDao(sql2o);
+        //CREATE
+        post("/users/new", "application/json", (req, res) -> {
+            Users user = gson.fromJson(req.body(), Users.class);
+            usersDao.add(user);
+            res.status(201);
+            return gson.toJson(user);
+        });
 
         //FILTERS
         exception(ApiException.class, (exception, req, res) -> {
